bf7c94d6aa4d514aa1960c88938ed6d5ee13032b
Chris Ryana <kissunchic@yandex.ru>
2020-07-17 22:21:06 +0300

    Добавлена production сборка

--- src/blocks/filters/filters.js
+++
@@ -1,8 +0,0 @@
-$('.filters__tongue').on('click', toggleFilters);
-
-function toggleFilters(e) {
-  e.preventDefault();
-  $(e.currentTarget)
-    .closest('.filters')
-    .toggleClass('filters--visible')
-}
--- src/pages/search/search.js
+++
@@ -1,3 +0,0 @@
-import '../../blocks/range-slider/range-slider';
-import '../../blocks/room-card/room-card';
-import '../../blocks/filters/filters';
